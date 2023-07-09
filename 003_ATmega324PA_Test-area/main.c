49c5d827128446d6f81c625902867372934ec900
bmendyk <bartlomiej.men@interia.eu>
2023-07-09 12:46:15 +0200

    First commit main.c for Atmega324PA, template only

---
+++ 003_ATmega324PA_Test-area/main.c
@@ -0,0 +1,37 @@
+/*
+ * main.c
+ *
+ *  Created on: 9 lip 2023
+ *      Author: bmendyk
+ */
+
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+
+
+	//Dierect Pin Ports
+	DDRB =(1<<PB5);
+	DDRC = (1<<PC0)|(1<<PC1)|(1<<PC2)|(1<<PC3);
+
+	//sei();
+
+	while(1)
+	{
+
+		PORTB ^= (PB5);
+		_delay_ms(1000);
+
+	}
+}
+
+ISR(TIMER0_OVF_vect)
+{
+
+}
